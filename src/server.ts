b4c63deaff6e90f85dfcee1c9f64d58f14b5854d
Guilherme <guilherme1067@hotmail.com>
2020-10-17 10:41:06 -0300

    created controllers and models

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,12 @@
 import express from 'express';
-
 import './database/connections';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-// Rota = conjunto
-// Recurso = usuario
 
-// Métodos HTTP = GET, POST, PUT, DELETE
-// Parâmetros
-
-// Query Params: http://loclhost:3333/users?search=gui
-// Route Params: http://loclhost:3333/users/1 (Identificar um recurso)
-// Body: http://loclhost:3333/users/1 (Identificar um recurso)
-
-app.get('/users', (request,response) =>{
-    return response.json("hello world")
-})
+app.use(routes);
 
 app.listen(3333)    
 
